56db4656c79a25b23d28b98a7a318983d170e460
tawadros312 <50532484+tawadros312@users.noreply.github.com>
2020-03-21 15:52:10 +0200

    Update hw1.py

--- hw1.py
+++ hw1.py
@@ -1,7 +1,10 @@
-dfe fib (x):
-a,b=0,1
-print(a+" ")
-for i in range (0,x+1)
-print(b+" ")
-c=a+b
-a,b=b,c
+def fib (x):
+ a,b=0,1
+ for i in range(1,x+1):
+  if (i == x):
+   return b
+  c=a+b
+  a,b=b,c
+print(fib(2))
+print(fib(5))
+print(fib(12))
